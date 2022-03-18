0771fa85714efb4e846b69bc1d003b7f7434f026
Viyan <viyan@phora.xyz>
2022-03-18 10:07:36 +0530

    Ascending Order Program

---
+++ study/Programming/ascending.lua
@@ -0,0 +1,18 @@
+list = {27,70,43,13,59,3}
+length = (#list)
+x = list
+for l = 1, length - 1 do
+for i = 1, length - l do
+if (list[i] > list[i + 1]) 
+then list[i], list[i + 1] = list[i + 1], list[i]
+	end
+end
+end
+	for i, x in ipairs(list) do
+		print(i, x)
+	end
+
+
+
+
+

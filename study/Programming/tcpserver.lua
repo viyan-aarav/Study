62fb5aa5983aadba311334ebde16f927954420be
Viyan <viyan@phora.xyz>
2022-05-24 20:50:37 +0530

    tcp server and client (command line)

--- study/Programming/tcpserver.lua
+++ study/Programming/tcpserver.lua
@@ -1,12 +1,11 @@
-local x = 0
 local counter = 0
 local socket = require 'socket.core'
 local server = socket.tcp()
 server:bind('*',arg[1])
 server:listen(31)
 
-local table = {}
-
+local tcpmap = {}
+local messagemap = {}
 print"listening"
 
 while 1 do
@@ -14,35 +13,27 @@ while 1 do
   local client = server:accept ()
   local msg = "client number\n"
  
-  local lin = client:receive()
-  local lin2 = client:receive()
-  local lin3 = client:receive()
-  print("received: " .. lin)
-while true  do
-  x = x + 1
+  local clientname = client:receive()
+  local destination = client:receive()
+  local message = client:receive()
+  print("received from: " .. clientname)
   counter = counter + 1
   print("sending: " ..msg)
   print("received: message details ")
   client:send("you are client number: \n")
   client:send(tostring(counter) .. "\n")
- client:send("send to & message: \n")
- client:send("from:\n")
-table[lin] = lin2
-print(lin2)
-print(lin3)
-function wait(second)
+  client:send("send to & message: \n")
+  client:send("from:\n")
+  messagemap[clientname] = message
+  tcpmap[clientname] = client
+  destclient = tcpmap[destination]
+  destmessage = messagemap[destination]
+  if destclient == nil  then
+     local dummy = 0
+  else
+     destclient:send(message)
+     client:send(destmessage)
+     break
+  end
 end
-if lin == max then
-repeat
-wait(5)
-until(lin == steve)
-else
-client:send(lin .. "\n")
-client:send(lin3 .. "\n")
--- print(table[lin])
-  --  break
-   break
- end
-end
-end
-end
+
